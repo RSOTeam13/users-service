8cd1011a736eb9c7af40b78be572cf16e47494c3
Anze Mur <am3158@student.uni-lj.si>
2021-01-06 00:50:38 +0100

    Test

--- api/src/main/java/si/fri/rso/albify/usersservice/api/v1/UsersServiceApplication.java
+++ api/src/main/java/si/fri/rso/albify/usersservice/api/v1/UsersServiceApplication.java
@@ -1,9 +1,19 @@
 package si.fri.rso.albify.usersservice.api.v1;
 
+import si.fri.rso.albify.usersservice.services.filters.AuthenticationFilter;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/v1")
 public class UsersServiceApplication extends Application {
 
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> resources = new HashSet<Class<?>>();
+        resources.add(AuthenticationFilter.class);
+        return resources;
+    }
 }
